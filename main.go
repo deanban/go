ed51795bacb13ea1d11fc219f866547beddeefa6
deanban <pbp2005@gmail.com>
2019-08-31 12:21:07 -0400

    first build

--- main.go
+++ main.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hi there!")
 }
+
+/****************************************************
+go cli
+
+go build 		--> Compiles a bunch of go source files
+go run 			--> Compiles one or two files
+go fmt 			--> Formars all the code in each file in the current directory
+go install 	--> Compiles and installs a package
+go get 			--> Downloads the raw source code of someone elses package
+go test 		--> Runs any test associated with the current project
+*****************************************************/
