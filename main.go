f22483cfbcf0b5047de223c87921eb61f6757ce2
deanban <pbp2005@gmail.com>
2019-08-31 12:35:55 -0400

    add package descriptions

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+//package is a workspace or like a folder
+//so if I had more files belonging to this workspace
+//I'd need all of them to include package main
+
+//in go there are 2 kinds of packages
+//1. executable package --> generates an executable file
+//2. reusabe package --> code used as 'helpers' or reusable logic
+
+//specifically 'package main' is used to make an executable type package
+//'package some-random-str' is used to make a reusabe type package. When run 'go build' it doesn't create an executable file.
+
 import "fmt"
 
+//if 'package main' is declared, the file must have a func called main()
 func main() {
 	fmt.Println("Hi there!")
 }
